2db0e2c58facb79f96ddd06f7dc466bd48bdcf2c
Kalimalieva <105165874+Kalimalieva@users.noreply.github.com>
2023-11-22 19:38:21 +0600

    object

---
+++ script.js
@@ -0,0 +1,95 @@
+
+// let n = 123;
+// n = 12.345;
+// n = 15
+// console.log(n)
+//
+// console.log(2/0)
+//
+// console.log("hi" / 2 )
+
+
+// let str = "hello"
+// console.log(str)
+// let str1 = 'одинарные кавычки'
+// console.log(str1)
+// let str2  = `обратные кавычки позволяют встраивать переменные ${str}`
+// console.log(str2)
+
+
+// let name = "Uluk"
+// console.log(`Hello, ${name}!`)
+
+// let check = true;
+// console.log(check)
+// let fal = false;
+// console.log(fal)
+
+// let number = 4 > 1
+// console.log(number)
+//
+// let num = 4 < 1
+// console.log(num)
+
+// let age = null
+// console.log(age)
+//
+// let age1
+// console.log(age1)
+//
+// console.log(typeof (5))
+// console.log(typeof 10n)
+// console.log(typeof true)
+// console.log(typeof "hello")
+// console.log(typeof Symbol("id"))
+// console.log(typeof null)
+// console.log(typeof alert)
+// console.log(typeof Math)
+
+
+// let str = "hello"
+// console.log(typeof str)
+
+// let number = 8
+// number = -number;
+// console.log(number)
+//
+// let num = 3
+// let num2 = 6
+// console.log( num + num2)
+
+// console.log(5 % 3)
+// console.log(8 % 3)
+// console.log(8 % 4)
+//
+// console.log(5 ** 2)
+//
+// let str = "I" + "am"
+// console.log(str)
+
+// console.log("4" + 4)
+// console.log("4" + 7)
+//
+// console.log(3 + 5 + "8")
+
+// let x = 1
+// console.log(+x)
+//
+// console.log(+true)
+// console.log(+"")
+//
+// let apple = "5"
+// let orange = "6"
+// console.log(apple + orange)
+//
+// let apple = "5"
+// let orange = "6"
+// console.log(+apple + +orange)
+
+let counter = 2// 2+1
+++counter// инкремент увеличивает значение на 1
+console.log(counter)
+
+let counter1 = 5//5-1
+--counter1// декремент уменьшает значение на 1
+console.log(counter1)
